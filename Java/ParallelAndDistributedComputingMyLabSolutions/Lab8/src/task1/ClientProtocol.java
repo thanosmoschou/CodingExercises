1ad633795f75cc41bedc1af2b8bb85d2d3334116
thanosmoschou <130168149+thanosmoschou@users.noreply.github.com>
2024-08-16 13:07:21 +0300

    add lab8

---
+++ Java/ParallelAndDistributedComputingMyLabSolutions/Lab8/src/task1/ClientProtocol.java
@@ -0,0 +1,38 @@
+package task1;
+
+import java.util.Scanner;
+
+public class ClientProtocol
+{
+    private Scanner keyboard = new Scanner(System.in);
+
+    private void printPrompt()
+    {
+        System.out.println("************************************");
+        System.out.println("AVAILABLE MODES:");
+        System.out.println("1. CONVERT UPPERCASE CHARACTERS TO LOWERCASE. FOR THIS MODE JUST ENTER SOME TEXT.");
+        System.out.println("2. CONVERT LOWERCASE CHARACTERS TO UPPERCASE. FOR THIS MODE JUST ENTER SOME TEXT.");
+        System.out.println("3. CEASAR'S CYPHER ENCRYPTION. FOR THIS MODE FIRST ENTER SOME TEXT AND THEN ENTER THE ENCRYPTION KEY (OFFSET)");
+        System.out.println("4. CEASAR'S CYPHER DECRYPTION. FOR THIS MODE FIRST ENTER SOME ENCRYPTED TEXT AND THEN ENTER ΤΗΕ DECRYPTION KEY (OFFSET).");
+        System.out.println("ENTER YOUR MESSAGE WITH THE FOLLOWING FORMAT: MODE, YOUR TEXT, AND YOUR ENCRYPTION/DECRYPTION KEY (IF ANY).");
+        System.out.println("TO EXIT THE APP JUST SIMPLY ENTER THE WORD EXIT...");
+        System.out.println("INPUT EXAMPLE: 3, HeLLo, 24");
+        System.out.println("ANOTHER INPUT EXAMPLE: 2, HELLO");
+    }
+
+    public String prepareRequest()
+    {
+        String msg;
+
+        printPrompt();
+        System.out.print("> ");
+        msg = keyboard.nextLine();
+
+        return msg;
+    }
+
+    public void processReply(String reply)
+    {
+        System.out.println("Reply from server: " + reply + "\n");
+    }
+}
---
+++ Java/ParallelAndDistributedComputingMyLabSolutions/Lab8/src/task1/Server.java
@@ -0,0 +1,46 @@
+/*
+Author: Thanos Moschou
+Description: Parallel And Distributed Computing Lab Solutions...
+ */
+
+package task1;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    private static final int PORT = 9090;
+
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+
+        //iterative server...in next labs I will make a multithreading one...
+        while (true)
+        {
+            Socket connectionSocket = serverSocket.accept();
+
+            InputStream is = connectionSocket.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+
+            OutputStream os = connectionSocket.getOutputStream();
+            PrintWriter out = new PrintWriter(os, true);
+
+            String inpMsg, outMsg;
+
+            ServerProtocol app = new ServerProtocol();
+
+            inpMsg = in.readLine();
+            while (!inpMsg.equals("EXIT"))
+            {
+                outMsg = app.getRequestAndReturnReply(inpMsg);
+                out.println(outMsg);
+                inpMsg = in.readLine();
+            }
+
+            connectionSocket.close();
+        }
+    }
+}
