71dd4aea04b8e7ba8df19ea46cc320c646750f5a
thanosmoschou <thanosmoschou054@gmail.com>
2024-09-09 18:40:12 +0300

    leetcode changes

--- LeetCode/CodingProblems/twoSum.c
+++ LeetCode/CodingProblems/twoSum.c
@@ -130,3 +130,34 @@ int* twoSum(int *nums, int numsSize, int target, int *returnSize)
     return NULL;
 }
 
+/*
+For some reason C code does not run...as a result I provide a Java solution below...
+*/
+
+class Solution 
+{
+    public int[] twoSum(int[] nums, int target)
+    {
+        int[] indexes = new int[2];
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            int fixNum = nums[i];
+            int complement = target - fixNum;
+            indexes[0] = i;
+
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[j] == complement)
+                {
+                    indexes[1] = j;
+                    return indexes;
+                }
+            }
+        }
+
+        return indexes;
+
+    }
+}
+
