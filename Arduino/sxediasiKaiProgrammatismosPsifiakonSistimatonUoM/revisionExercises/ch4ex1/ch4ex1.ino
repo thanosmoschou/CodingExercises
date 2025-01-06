94fefefbc2909e44d0fe318730066c34728d236e
thanosmoschou <130168149+thanosmoschou@users.noreply.github.com>
2025-01-06 13:56:24 +0200

    new revision exercises

---
+++ Arduino/sxediasiKaiProgrammatismosPsifiakonSistimatonUoM/revisionExercises/ch4ex1/ch4ex1.ino
@@ -0,0 +1,31 @@
+/*
+Author: Thanos Moschou
+Description: Ch4 ex1
+
+Components:
+1 9V Battery
+1 5V Regulator
+3 Resistors 220 Ohm
+1 ATTiny85
+3 Leds
+*/
+
+int pins[] = {0, 1, 2}; // PB0, PB1, PB2
+int length = 3;
+
+void setup()
+{
+  for (int i = 0; i < length; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
+}
+
+void loop()
+{
+  for (int i = 0; i < length; i++) {
+    digitalWrite(pins[i], HIGH);
+    delay(500);
+    digitalWrite(pins[i], LOW);
+    delay(500);
+  }
+}
